82dd1655d54059b4c381d307c9ed3ffcbbef2520
yash-metron <104358124+yash-metron@users.noreply.github.com>
2024-04-16 18:22:30 +0530

    Digital shadows 2.1.1 (#9)
    * Updated the label, label which are present in config
    
    * Added the release_notes for version 2.1.1
    
    * Updated the minimum prodcut version from 5.3.5 to 6.1.1
    
    * Updated minimum phantom version.
    
    * Updating minimum product version to 5.5.0
    
    * Updating minimum product version to 6.1.1
    
    * standard dev checks
    
    * updated certifi library
    
    * standard dev changes
    
    * minor changes
    
    * use platform certifi library and minor standard checks
    
    * added certifi
    
    * removed certifi
    
    * reverted min phantom version and added release notes
    
    * updated release notes
    
    ---------
    
    Co-authored-by: mishalp-crest <mishalp@splunk.com>

--- digital_shadows_connector.py
+++ digital_shadows_connector.py
@@ -172,20 +172,71 @@ class DigitalShadowsConnector(BaseConnector):
 
 if __name__ == '__main__':
 
-    import sys
+    import argparse
+    from sys import exit
 
-    if len(sys.argv) < 2:
-        print("No test json specified as input")
-        sys.exit(0)
+    import pudb
+    import requests
 
-    with open(sys.argv[1]) as f:
+    pudb.set_trace()
+
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+    verify = args.verify
+
+    if username is not None and password is None:
+
+        # User specified a username but not a password, so ask
+        import getpass
+        password = getpass.getpass("Password: ")
+
+    if username and password:
+        try:
+            login_url = '{}/login'.format(DigitalShadowsConnector._get_phantom_base_url())
+
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=verify, timeout=30)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken={}'.format(csrftoken)
+            headers['Referer'] = login_url
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=verify, data=data, headers=headers, timeout=30)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print("Unable to get session id from the platform. Error: {}".format(str(e)))
+            exit(1)
+
+    with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=4))
 
         connector = DigitalShadowsConnector()
         connector.print_progress_message = True
+
+        if session_id is not None:
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    sys.exit(0)
+    exit(0)
--- release_notes/unreleased.md
+++ release_notes/unreleased.md
@@ -1,1 +1,3 @@
 **Unreleased**
+* Updated 'on poll' action to add container's label
+* Updated certifi dependencies in order to use platform packages
